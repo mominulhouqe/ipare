20f84950a09552ce399a031bb19a9af931b01e7f
mominul <ulhouqemomin39@gmail.com>
2024-02-15 17:09:14 +0600

    Added navber

--- src/components/Layouts/Navber.tsx
+++ src/components/Layouts/Navber.tsx
@@ -4,16 +4,16 @@ import { Button } from "../ui/button";
 const Navber = () => {
   return (
     <header className="h-12">
-      <nav className="w-full max-w-[1220px] px-[20px] bg-green-300 h-full  flex justify-between items-center mx-auto">
-        <span>Ipare</span>
+      <nav className="w-full max-w-[1220px] px-[20px] h-full  flex justify-between items-center mx-auto">
+        <span className="text-3xl">Ipare</span>
         <ul className="space-x-5">
           <NavLink to="/">Home</NavLink>
 
           <NavLink to="/about">About</NavLink>
 
-          <NavLink to="/">
+          
             <Button>Login</Button>
-          </NavLink>
+        
         </ul>
       </nav>
     </header>
