8d128cc67db9cb1acfefb1737fa45f25ef6468ce
mominul <ulhouqemomin39@gmail.com>
2024-02-16 15:22:34 +0600

    Added animation in hero section

--- src/components/Layouts/Navber.tsx
+++ src/components/Layouts/Navber.tsx
@@ -1,9 +1,17 @@
 import { NavLink } from "react-router-dom";
 import { Button } from "../ui/button";
+import { motion } from "framer-motion";
 
 const Navber = () => {
   return (
-    <header className="h-12">
+    <motion.header 
+    initial={{opacity:0}}
+    animate={{opacity:1}}
+    transition={{
+      delay:1,
+      duration:1,
+    }}
+    className="h-12 bg-white fixed w-full z-50">
       <nav className="w-full max-w-[1230px] px-[20px] h-full  flex justify-between items-center mx-auto">
         <span className="text-3xl">Ipare</span>
         <ul className="space-x-5">
@@ -14,7 +22,7 @@ const Navber = () => {
           <Button>Login</Button>
         </ul>
       </nav>
-    </header>
+    </motion.header>
   );
 };
 
--- src/pages/Home/HeroSection.tsx
+++ src/pages/Home/HeroSection.tsx
@@ -1,29 +1,95 @@
 import Container from "@/components/ui/Container";
 import { Button } from "@/components/ui/button";
-
+import { motion } from "framer-motion";
 const HeroSection = () => {
+  const intro = {
+    hidden: {
+      opacity: 0,
+    },
+    visible: {
+      opacity: 1,
+      transition: {
+        duration: 0.5,
+        staggerChildren: 0.25,
+        delayChildren: 1,
+      },
+    },
+  };
+
+  const introChildren = {
+    hidden: {
+      opacity: 0,
+      y: -200,
+    },
+    visible: {
+      opacity: 1,
+
+      y: 0,
+      transition: {
+        duration: 1,
+        type: "spring",
+        bounce: 0.5,
+      },
+    },
+  };
+
+  const latop = {
+    initial: {
+      y: 0,
+      scale: 5,
+      rotate: 0,
+    },
+    animate: {
+      y: 20,
+      rotate: -30,
+      scale: 1,
+      transition: {
+        duration: 1,
+        y: {
+          duration: 2,
+          repeat: Infinity,
+          repeatType: "reverse",
+          ease: "easeInOut",
+        },
+      },
+    },
+  };
+
   return (
-    <Container className="h-[calc(100vh-64px)] grid grid-cols-1 md:grid-cols-2 place-items-center gap-6 mx-auto">
-      <div>
-        <h1 className="lg:text-7xl text-4xl  font-bold mb-6">
-          <span className="text-gray">Don't worry ,</span>
-          <br />
-          <span>We will fix it.</span>
-        </h1>
-        <p className="text-dark-gray mb-4">
-          Lorem ipsum <span>dolor</span> sit amet consectetur, adipisicing elit.
-          Impedit voluptate accusamus quas <span>eaque</span> cumque! Eum.
-        </p>
-        <Button>Book a Service</Button>
-      </div>
-      <div className="w-3/4 md:max-w-sm mx-auto">
-        <img
-          className="-rotate-[30deg] w-full "
-          src="https://i.ibb.co/7YVWVhq/fotor-ai-20230920171736.jpg"
-          alt=""
-        />
-      </div>
-    </Container>
+    <div className="overflow-hidden">
+      <Container className="h-screen grid grid-cols-1 md:grid-cols-2 place-items-center gap-6 mx-auto  ">
+        <motion.div variants={intro} initial="hidden" animate="visible">
+          <motion.h1
+            variants={introChildren}
+            className="lg:text-7xl text-4xl  font-bold mb-6"
+          >
+            <span className="text-gray">Don't worry ,</span>
+            <br />
+            <span>We will fix it.</span>
+          </motion.h1>
+          <motion.p variants={introChildren} className="text-dark-gray mb-4">
+            Lorem ipsum <span>dolor</span> sit amet consectetur, adipisicing
+            elit. Impedit voluptate accusamus quas <span>eaque</span> cumque!
+            Eum.
+          </motion.p>
+          <motion.div variants={introChildren}>
+            <Button>Book a Service</Button>
+          </motion.div>
+        </motion.div>
+        <motion.div
+          variants={latop}
+          initial="initial"
+          animate="animate"
+          className="w-3/4 md:max-w-sm mx-auto"
+        >
+          <img
+            className=" w-full "
+            src="https://i.ibb.co/7YVWVhq/fotor-ai-20230920171736.jpg"
+            alt=""
+          />
+        </motion.div>
+      </Container>
+    </div>
   );
 };
 
