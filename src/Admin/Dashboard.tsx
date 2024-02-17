d342bdcb61118904ae785867f8a4fe6d51f81d3e
mominul <ulhouqemomin39@gmail.com>
2024-02-17 16:08:12 +0600

    Added Tanstak query and data fetching

--- src/Admin/Dashboard.tsx
+++ src/Admin/Dashboard.tsx
@@ -1,10 +1,37 @@
+import { getUsers } from "@/apis/Admin/Services/Services";
+import { useQuery } from "@tanstack/react-query";
 
 const Dashboard = () => {
-    return (
-        <div className="bg-green-300">
-            This is admin dashboard Home
-        </div>
-    );
+  //   const [user, setUser] = useState([]);
+  //   console.log(user);
+
+  //   useEffect(() => {
+  //     fetch("https://jsonplaceholder.typicode.com/users")
+  //       .then((res) => res.json())
+  //       .then((data) => setUser(data));
+  //   }, []);
+
+
+
+
+  const { data, isError, isLoading } = useQuery({
+    queryKey: ["service"],
+    queryFn:  getUsers,
+  });
+  if (isLoading) {
+    return <p>Loading</p>;
+  }
+// console.log({data});
+
+  return <div className="bg-green-300">
+    {
+        data.map((user) =>(
+            <div key={user.id}>
+                {user.name}
+            </div>
+        ))
+    }
+  </div>;
 };
 
 export default Dashboard;
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import { RouterProvider } from 'react-router-dom'
-import router from './routes/routes.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import router from "./routes/routes.tsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router}></RouterProvider>
-  </React.StrictMode>,
-)
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router}></RouterProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  </React.StrictMode>
+);
